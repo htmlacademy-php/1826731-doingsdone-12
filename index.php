72393839dd092515ae0386a447e0807181385f5a
Shishkeks <shishalov_yura@mail.ru>
2022-03-25 20:54:20 +0300

    Добавил массив

--- index.php
+++ index.php
@@ -125,7 +125,7 @@ $tasks =
                                 <td class="task__select">
                                     <label class="checkbox task__checkbox">
                                         <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1">
-                                        <span class="checkbox__text"><?= $task['title']; ?></span>
+                                            <span class="checkbox__text"><?= $task['title']; ?></span>
                                     </label>
                                 </td>
                                 <td class="task__file">
